8bab8af706e9fc06099c2a502e6c8fe178f2024c
Sharad <sharadd19@gmail.com>
2024-06-28 18:35:38 +1000

    added ability to get membership

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,5 +1,6 @@
 var asyncHandler = require("express-async-handler");
 var PostModel = require("../models/postModel");
+var UserModel = require("../models/userModel");
 
 exports.userHome = asyncHandler(async (req, res) => {
   const postList = await PostModel.find().exec();
@@ -21,18 +22,24 @@ exports.getPostsByUser = asyncHandler(async (req, res) => {
   });
 });
 
-exports.getMembershipForm = (req, res) => {
-  res.render("membershipForm", { title: "Membership" });
-};
+exports.getMembershipForm = asyncHandler(async(req, res) => {
+  const {membership} = await UserModel.findOne({_id: req.user.id}, "membership").exec()
+  
+  res.render("membershipForm", { title: "Membership", membership: membership });
+});
 
 exports.membership = asyncHandler(async (req, res) => {
   const secret = req.body.secret;
+  const postList = await PostModel.find().exec();
+ 
   if (secret === "excelsior") {
-    await UserModel.findByIdAndUpdate(req.user.id, { membership: true });
+    await UserModel.findByIdAndUpdate(req.user.id, {membership: true});
+
+    res.render("user", { user: req.user, postList: postList });
   } else {
-    res.redirect("/user/membership", {
+    res.render("membershipForm", {
       title: "Membership",
-      error: {val: true, message: "Sorry, you entered the wrong secret"},
+      error: { val: true, message: "Sorry, you entered the wrong secret" },
     });
   }
 });
